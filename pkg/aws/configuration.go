60f7db33c7404cf6fa9a3579ba6504d04dc01d15
Leapfrog <leapfrog@pop-os.localdomain>
2022-05-23 16:49:07 +0545

    AWS configuration and listing objects

---
+++ pkg/aws/configuration.go
@@ -0,0 +1,18 @@
+package aws
+
+import (
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/credentials"
+	"github.com/aws/aws-sdk-go-v2/service/s3"
+)
+
+func GetS3Client(assessKey string, secretKey string, region string) *s3.Client {
+	opts := s3.Options{
+		Region:      *aws.String(region),
+		Credentials: aws.NewCredentialsCache(credentials.NewStaticCredentialsProvider(assessKey, secretKey, "")),
+	}
+	// Create an Amazon S3 service client
+	client := s3.New(opts)
+
+	return client
+}
---
+++ pkg/aws/services/s3/s3_service.go
@@ -0,0 +1,27 @@
+package s3
+
+import (
+	"context"
+	"path/filepath"
+
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/service/s3"
+)
+
+func ListFiles(client *s3.Client, bucket *string) (*s3.ListObjectsV2Output, error) {
+
+	input := &s3.ListObjectsV2Input{
+		Bucket: bucket,
+	}
+	return client.ListObjectsV2(context.Background(), input)
+}
+
+func GetFile(client *s3.Client, bucket *string, prefix string, name string) (*s3.GetObjectAclOutput, error) {
+
+	key := filepath.Join(prefix, name)
+	input := &s3.GetObjectAclInput{
+		Bucket: aws.String(*bucket),
+		Key:    aws.String(key),
+	}
+	return client.GetObjectAcl(context.Background(), input)
+}
