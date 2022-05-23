1115e6e7987eeae176ea9d46b47d75c26f3d57d0
Sudip Katwal <sudipkatwal@lftechnology.com>
2022-05-23 16:51:12 +0545

    typo change

--- pkg/aws/services/s3/s3_service.go
+++ pkg/aws/services/s3/s3_service.go
@@ -13,15 +13,17 @@ func ListFiles(client *s3.Client, bucket *string) (*s3.ListObjectsV2Output, erro
 	input := &s3.ListObjectsV2Input{
 		Bucket: bucket,
 	}
+
 	return client.ListObjectsV2(context.Background(), input)
 }
 
 func GetFile(client *s3.Client, bucket *string, prefix string, name string) (*s3.GetObjectAclOutput, error) {
-
 	key := filepath.Join(prefix, name)
+
 	input := &s3.GetObjectAclInput{
 		Bucket: aws.String(*bucket),
 		Key:    aws.String(key),
 	}
+
 	return client.GetObjectAcl(context.Background(), input)
 }
