fe227b0c77cac1d96e37fedfa01090561dfc4a5e
rachitamaharjan <smilerac15@gmail.com>
2022-05-26 14:24:11 +0545

    minor changes

--- pkg/network/google_sheets/data.go
+++ pkg/network/google_sheets/data.go
@@ -1,5 +1,19 @@
 package google_sheets
 
+//GoogleCreds google service account credentials
+type GoogleCreds struct {
+	Type            string `json:"type"`
+	ProjectID       string `json:"project_id"`
+	PrivateKeyID    string `json:"private_key_id"`
+	PrivateKey      string `json:"private_key"`
+	ClientID        string `json:"client_id"`
+	ClientEmail     string `json:"client_email"`
+	AuthURI         string `json:"auth_uri"`
+	TokenURI        string `json:"token_uri"`
+	ProviderCertURI string `json:"auth_provider_x509_cert_url"`
+	ClientCertURI   string `json:"client_x509_cert_url"`
+}
+
 type FileMetaData struct {
 	ResourceKey       string            `json:"resourceKey"`
 	LinkShareMetaData linkShareMetaData `json:"linkShareMetaData"`
