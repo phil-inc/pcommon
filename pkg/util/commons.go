91ae9c0491b4c10875d11def336752a3c0954763
Swechhya <swechhyabista@gmail.com>
2023-10-17 13:39:55 +0545

    Add snake case function

--- pkg/util/commons.go
+++ pkg/util/commons.go
@@ -993,7 +993,16 @@ func ConvertToRegexPattern(input string) string {
 	return fmt.Sprintf("^%s$", builder.String())
 }
 
-//Converts every first letter of the  word in the sentence to uppercase
+// Converts every first letter of the  word in the sentence to uppercase
 func TitleCase(input string) string {
 	return cases.Title(language.AmericanEnglish).String(input)
+}
+
+func SnakeCase(str string) string {
+	matchFirstCap := regexp.MustCompile("(.)([A-Z][a-z]+)")
+	matchAllCap := regexp.MustCompile("([a-z0-9])([A-Z])")
+
+	snake := matchFirstCap.ReplaceAllString(str, "${1}_${2}")
+	snake = matchAllCap.ReplaceAllString(snake, "${1}_${2}")
+	return strings.ToLower(snake)
 }
