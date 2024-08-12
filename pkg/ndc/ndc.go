5a41a7ea710d0835ea8200fb9740ce3313cd364e
hastagurung <gurungryu@gmail.com>
2024-08-12 16:21:47 +0545

    added func comments

--- pkg/ndc/ndc.go
+++ pkg/ndc/ndc.go
@@ -188,6 +188,7 @@ func addLeadingZero(part string) string {
 	return "0" + part
 }
 
+// SanitizeTo11Digit sanitize the ndc and add dash to format the NDC
 func SanitizeTo11Digit(s string) string {
 	sanitized := Sanitize(s)
 	return AddDashesTo11Digit(sanitized)
