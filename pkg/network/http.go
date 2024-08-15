c9c67b8275d3e319a87cc85765afb4f395659c47
Swechhya <swechhyabista@gmail.com>
2024-08-15 18:13:17 +0545

    Use circuit breaker in GET

--- pkg/network/http.go
+++ pkg/network/http.go
@@ -11,6 +11,8 @@ import (
 	"strconv"
 	"strings"
 	"time"
+
+	"github.com/phil-inc/pcommon/pkg/circuitbreaker"
 )
 
 type ErrorObject struct {
@@ -127,17 +129,22 @@ func HTTPGetWithTimeOut(url string, headers map[string]string, timeout int) ([]b
 // HTTPGet - makes a get request to the given URL and HTTP headers.
 // it returns response data byte or error
 func HTTPGet(url string, headers map[string]string) ([]byte, error) {
-	req, err := getNewRequest("GET", url, headers)
-	if err != nil {
-		return nil, err
-	}
+	config := circuitbreaker.GetCircuitBreakerConfig(url)
+	cb := circuitbreaker.NewCircuitBreaker(url, config.FailureThreshold, config.HalfOpenSuccess, config.OpenTimeout)
+	return cb.Call(func() ([]byte, error) {
+		req, err := getNewRequest("GET", url, headers)
+		if err != nil {
+			return nil, err
+		}
 
-	res, err := httpClient.Do(req)
-	if err != nil {
-		return nil, err
-	}
+		res, err := httpClient.Do(req)
+		if err != nil {
+			return nil, err
+		}
 
-	return parseGetResponse(res, url)
+		return parseGetResponse(res, url)
+
+	})
 }
 
 // HTTPGetWithBasicAuth - makes a get request to the given URL and HTTP headers With Basic Auth
