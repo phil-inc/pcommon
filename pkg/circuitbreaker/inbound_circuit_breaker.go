abc73c34980de9fa77dd395b23222ab677817970
Swechhya <swechhyabista@gmail.com>
2024-08-26 16:05:37 +0545

    Add additional function comments

--- pkg/circuitbreaker/inbound_circuit_breaker.go
+++ pkg/circuitbreaker/inbound_circuit_breaker.go
@@ -1,10 +1,19 @@
 package circuitbreaker
 
+// NewInboundCircuitBreaker creates and initializes a new BaseCircuitBreaker instance
+// for the specified inbound endpoint. It sets up the circuit breaker with configurations
+// retrieved from the inbound configuration map and loads its initial state from Redis.
+// Returns the newly created BaseCircuitBreaker instance.
 func NewInboundCircuitBreaker(endpoint string) *BaseCircuitBreaker {
 	return newCircuitBreaker(endpoint, true)
 }
 
-// SetInboundCircuitBreakerConfig sets the configuration for inbound calls.
+// SetInboundCircuitBreakerConfig sets the configuration for the circuit breaker associated with inbound calls
+// for a specific endpoint. It updates or adds the configuration in the `inboundConfigurations` map.
+// The configuration is locked during the update to ensure thread safety.
+// Parameters:
+//   - endpoint: The endpoint URL for which the configuration is being set.
+//   - config: The CircuitBreakerConfig object containing the configuration settings.
 func SetInboundCircuitBreakerConfig(endpoint string, config CircuitBreakerConfig) {
 	configMu.Lock()
 	defer configMu.Unlock()
@@ -13,7 +22,15 @@ func SetInboundCircuitBreakerConfig(endpoint string, config CircuitBreakerConfig
 	inboundConfigurations[endpoint] = config
 }
 
-// GetInboundCircuitBreakerConfig retrieves the configuration for inbound calls.
+// GetInboundCircuitBreakerConfig retrieves the configuration for the circuit breaker associated with inbound calls
+// for a specific endpoint. It returns the configuration from the `inboundConfigurations` map if it exists,
+// or a default configuration if no configuration is found for the endpoint.
+// The configuration is locked during the retrieval to ensure thread safety.
+// Parameters:
+//   - endpoint: The endpoint URL for which the configuration is being retrieved.
+//
+// Returns:
+//   - The CircuitBreakerConfig object for the specified endpoint. If no configuration is found, a default configuration is returned.
 func GetInboundCircuitBreakerConfig(endpoint string) CircuitBreakerConfig {
 	configMu.Lock()
 	defer configMu.Unlock()
--- pkg/circuitbreaker/outbound_circuit_breaker.go
+++ pkg/circuitbreaker/outbound_circuit_breaker.go
@@ -1,10 +1,19 @@
 package circuitbreaker
 
+// NewOutboundCircuitBreaker creates and initializes a new BaseCircuitBreaker instance
+// for the specified outbound endpoint. It sets up the circuit breaker with configurations
+// retrieved from the outbound configuration map and loads its initial state from Redis.
+// Returns the newly created BaseCircuitBreaker instance.
 func NewOutboundCircuitBreaker(endpoint string) *BaseCircuitBreaker {
 	return newCircuitBreaker(endpoint, false)
 }
 
-// SetOutboundCircuitBreakerConfig sets the configuration for outbound calls.
+// SetOutboundCircuitBreakerConfig sets the configuration for the circuit breaker associated with outbound calls
+// for a specific endpoint. It updates or adds the configuration in the `outboundConfigurations` map.
+// The configuration is locked during the update to ensure thread safety.
+// Parameters:
+//   - endpoint: The endpoint URL for which the configuration is being set.
+//   - config: The CircuitBreakerConfig object containing the configuration settings.
 func SetOutboundCircuitBreakerConfig(endpoint string, config CircuitBreakerConfig) {
 	configMu.Lock()
 	defer configMu.Unlock()
@@ -13,7 +22,15 @@ func SetOutboundCircuitBreakerConfig(endpoint string, config CircuitBreakerConfi
 	outboundConfigurations[endpoint] = config
 }
 
-// GetOutboundCircuitBreakerConfig retrieves the configuration for outbound calls.
+// GetOutboundCircuitBreakerConfig retrieves the configuration for the circuit breaker associated with outbound calls
+// for a specific endpoint. It returns the configuration from the `outboundConfigurations` map if it exists,
+// or a default configuration if no configuration is found for the endpoint.
+// The configuration is locked during the retrieval to ensure thread safety.
+// Parameters:
+//   - endpoint: The endpoint URL for which the configuration is being retrieved.
+//
+// Returns:
+//   - The CircuitBreakerConfig object for the specified endpoint. If no configuration is found, a default configuration is returned.
 func GetOutboundCircuitBreakerConfig(endpoint string) CircuitBreakerConfig {
 	configMu.Lock()
 	defer configMu.Unlock()
