a2fbd48bd9e8f6c155e711d916da143898a39f2f
Swechhya <swechhyabista@gmail.com>
2025-09-08 16:11:53 -0700

    Upadate for mock

--- pkg/pgorm/internal/mock_executor.go
+++ pkg/pgorm/internal/mock_executor.go
@@ -2,45 +2,26 @@ package internal
 
 import "database/sql"
 
-type sqlRowsWrapper struct {
-	rows *sql.Rows
+type MockDBExecutor struct {
+	DB *sql.DB
 }
 
-func (r *sqlRowsWrapper) Next() bool {
-	return r.rows.Next()
+func (m *MockDBExecutor) Exec(query string, args ...interface{}) (int64, error) {
+	res, err := m.DB.Exec(query, args...)
+	if err != nil {
+		return 0, err
+	}
+	return res.RowsAffected()
 }
 
-func (r *sqlRowsWrapper) Scan(dest ...interface{}) error {
-	return r.rows.Scan(dest...)
-}
-
-func (r *sqlRowsWrapper) Columns() ([]string, error) {
-	return r.rows.Columns()
-}
-
-func (r *sqlRowsWrapper) Close() error {
-	return r.rows.Close()
-}
-
-func (r *sqlRowsWrapper) Err() error {
-	return r.rows.Err()
-}
-
-func (r *sqlRowsWrapper) Values() ([]interface{}, error) {
-	columns, err := r.rows.Columns()
+func (m *MockDBExecutor) Query(query string, args ...interface{}) (Rows, error) {
+	rows, err := m.DB.Query(query, args...)
 	if err != nil {
 		return nil, err
 	}
+	return &sqlRowsWrapper{rows}, nil
+}
 
-	values := make([]interface{}, len(columns))
-	pointers := make([]interface{}, len(columns))
-	for i := range values {
-		pointers[i] = &values[i]
-	}
-
-	if err := r.rows.Scan(pointers...); err != nil {
-		return nil, err
-	}
-
-	return values, nil
+func (m *MockDBExecutor) QueryRow(query string, args ...interface{}) Row {
+	return m.DB.QueryRow(query, args...)
 }
